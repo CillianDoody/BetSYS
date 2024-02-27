59b3ca0ab2fa1e84ef2d79b02f6e8ffe4b6a9239
CillianDoody <158101689+CillianDoody@users.noreply.github.com>
2024-02-27 12:54:16 +0000

    Add files via upload

--- cd.sql
+++ cd.sql
@@ -1,3 +1,5 @@
+DROP TABLE Fixtures;
+DROP TABLE Teams;
 DROP TABLE Customers;
 
 CREATE TABLE Customers(
@@ -11,3 +13,20 @@ Postcode varchar2(7) NOT NULL,
 Status char(1) NOT NULL, 
 Balance numeric(7,2) DEFAULT 0,
 CONSTRAINT PK_AccountID PRIMARY KEY (AccountID));
+
+CREATE TABLE Teams(
+TeamID varchar2(3) NOT NULL,
+TeamName varchar2(25) NOT NULL,
+CONSTRAINT PK_TeamID PRIMARY KEY (TeamID));
+
+CREATE TABLE Fixtures(
+FixtureID varchar2(10) NOT NULL,
+Team1 varchar2(25) NOT NULL,
+Team2 varchar2(25) NOT NULL,
+OddsTeam1 varchar2(25) NOT NULL,
+OddsTeam2 varchar2(25) NOT NULL,
+Fdate date NOT NULL,
+FTime TIMESTAMP NOT NULL,
+Score1 numeric(2),
+Score2 numeric(2),
+CONSTRAINT PK_FixtureID PRIMARY KEY (FixtureID));
