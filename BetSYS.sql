d0f8ad2475ae6e5504c0a59bbd60713c8b20e6a1
CillianDoody <158101689+CillianDoody@users.noreply.github.com>
2024-04-15 22:17:29 +0100

    Add files via upload

---
+++ BetSYS.sql
@@ -0,0 +1,59 @@
+--dropping tables
+DROP TABLE Bets;
+DROP TABLE Fixtures;
+DROP TABLE FixtureTimes;
+DROP TABLE Teams;
+DROP TABLE Customers;
+
+--Creating table Customers
+CREATE TABLE Customers(
+AccountID varchar2(10) NOT NULL,
+Forename varchar2(20) NOT NULL,
+Surname varchar2(20) NOT NULL,
+DOB date NOT NULL, 
+PhoneNumber varchar2(10), 
+Email varchar2(40) NOT NULL, 
+Postcode varchar2(7) NOT NULL, 
+Status char(1) NOT NULL, 
+Balance numeric(7,2) DEFAULT 0,
+CONSTRAINT PK_AccountID PRIMARY KEY (AccountID));
+
+--Creating table Teams
+CREATE TABLE Teams(
+TeamName varchar2(25) NOT NULL,
+CONSTRAINT PK_TeamName PRIMARY KEY (TeamName));
+
+--Creating table FixtureTimes
+CREATE TABLE FixtureTimes (
+FixtureTime char(5) NOT NULL,
+CONSTRAINT PK_FixtureTime PRIMARY KEY (FixtureTime));
+
+--Creating table Fixtures
+CREATE TABLE Fixtures(
+FixtureID varchar2(10) NOT NULL,
+Team1 varchar2(25) NOT NULL,
+Team2 varchar2(25) NOT NULL,
+OddsTeam1 numeric(4,2) NOT NULL,
+OddsTeam2 numeric(4,2) NOT NULL,
+Fdate date NOT NULL,
+FTime char(5) NOT NULL,
+Score1 numeric(2),
+Score2 numeric(2),
+CONSTRAINT PK_FixtureID PRIMARY KEY (FixtureID),
+FOREIGN KEY (FTime) REFERENCES FixtureTimes(FixtureTime),
+FOREIGN KEY (Team1) REFERENCES Teams(TeamName),
+FOREIGN KEY (Team2) REFERENCES Teams(TeamName));
+
+CREATE TABLE Bets(
+BetID varchar2(10) NOT NULL,
+BetAmmount numeric (5,2) NOT NULL,
+BetOdds numeric (4,2) NOT NULL,
+BetStatus char(1) NOT NULL,
+Team varchar2(25) NOT NULL,
+FixtureID varchar2(10) NOT NULL,
+AccountID varchar2(10) NOT NULL,
+FOREIGN KEY (Team) REFERENCES Teams(TeamName),
+FOREIGN KEY (FixtureID) REFERENCES Fixtures(FixtureID),
+FOREIGN KEY (AccountID) REFERENCES Customers(AccountID));
+
+COMMIT;
---
+++ PopulateTablesBetSYS.sql
@@ -0,0 +1,159 @@
+--Populating table Customers
+INSERT INTO Customers (AccountID, Forename, Surname, DOB, PhoneNumber,
+Email, Postcode, Status, Balance)
+VALUES
+('1', 'Cillian', 'Doody', '24-JUN-2002', '0874667264',
+'cillian@gmail.com', 'p51k1c6', 'a', 0.00);
+INSERT INTO Customers (AccountID, Forename, Surname, DOB, PhoneNumber,
+Email, Postcode, Status, Balance)
+VALUES
+('2', 'Samir', 'Boussoufa', '23-SEP-1942', '0854206969',
+'samir@gmail.com', 'v92y568', 'a', 0.00);
+INSERT INTO Customers (AccountID, Forename, Surname, DOB, PhoneNumber,
+Email, Postcode, Status, Balance)
+VALUES
+('3', 'Mark', 'Hennessey', '23-MAY-1942', '0837428254',
+'mark@gmail.com', 'v31xj45', 'a', 0.00);
+INSERT INTO Customers (AccountID, Forename, Surname, DOB, PhoneNumber,
+Email, Postcode, Status, Balance)
+VALUES
+('4', 'Jack', 'Leonard-Sexton', '10-MAR-2002', '0852062320',
+'jack@gmail.com', 'v94cyf8', 'a', 0.00);
+INSERT INTO Customers (AccountID, Forename, Surname, DOB, PhoneNumber,
+Email, Postcode, Status, Balance)
+VALUES
+('5', 'Lara', 'Krimmer', '07-MAY-2004', '0894771239',
+'lara@outlook.com', 'v92hh27', 'a', 0.00);
+INSERT INTO Customers (AccountID, Forename, Surname, DOB, PhoneNumber,
+Email, Postcode, Status, Balance)
+VALUES
+('6', 'Bryan', 'Murphy', '20-JAN-2003', '0834649095',
+'smurfyboy3000@gmail.com', 'v92na40', 'a', 0.00);
+
+--Populating table Teams.
+INSERT INTO Teams (TeamName)
+VALUES
+('Arsenal');
+INSERT INTO Teams (TeamName)
+VALUES
+('Aston Villa');
+INSERT INTO Teams (TeamName)
+VALUES
+('Bournemouth');
+INSERT INTO Teams (TeamName)
+VALUES
+('Brentford');
+INSERT INTO Teams (TeamName)
+VALUES
+('Brighton and Hove Albion');
+INSERT INTO Teams (TeamName)
+VALUES
+('Burnley');
+INSERT INTO Teams (TeamName)
+VALUES
+('Chelsea');
+INSERT INTO Teams (TeamName)
+VALUES
+('Crystal Palace');
+INSERT INTO Teams (TeamName)
+VALUES
+('Everton');
+INSERT INTO Teams (TeamName)
+VALUES
+('Fulham');
+INSERT INTO Teams (TeamName)
+VALUES
+('Liverpool');
+INSERT INTO Teams (TeamName)
+VALUES
+('Luton Town');
+INSERT INTO Teams (TeamName)
+VALUES
+('Manchester City');
+INSERT INTO Teams (TeamName)
+VALUES
+('Manchester United');
+INSERT INTO Teams (TeamName)
+VALUES
+('Newcastle United');
+INSERT INTO Teams (TeamName)
+VALUES
+('Nottingham Forest');
+INSERT INTO Teams (TeamName)
+VALUES
+('Sheffield United');
+INSERT INTO Teams (TeamName)
+VALUES
+('Tottenham Hotspur');
+INSERT INTO Teams (TeamName)
+VALUES
+('West Ham United');
+INSERT INTO Teams (TeamName)
+VALUES
+('Wolverhampton Wanderers');
+
+--Populating table FixtureTimes
+INSERT INTO FixtureTimes (FixtureTime)
+VALUES
+('12:30');
+INSERT INTO FixtureTimes (FixtureTime)
+VALUES
+('14:00');
+INSERT INTO FixtureTimes (FixtureTime)
+VALUES
+('15:00');
+INSERT INTO FixtureTimes (FixtureTime)
+VALUES
+('15:30');
+INSERT INTO FixtureTimes (FixtureTime)
+VALUES
+('16:30');
+INSERT INTO FixtureTimes (FixtureTime)
+VALUES
+('17:30');
+INSERT INTO FixtureTimes (FixtureTime)
+VALUES
+('20:00');
+
+--Populating table Fixtures.
+INSERT INTO Fixtures (FixtureID, Team1, Team2, OddsTeam1, OddsTeam2,
+Fdate, FTime, Score1, Score2)
+VALUES
+('1', 'Newcastle United', 'West Ham United', 1.40, 3.33,
+'30-MAR-2024', '12:30', '0', '0');
+INSERT INTO Fixtures (FixtureID, Team1, Team2, OddsTeam1, OddsTeam2,
+Fdate, FTime, Score1, Score2)
+VALUES
+('2', 'Bournemouth', 'Everton', 1.69, 2.50,
+'30-MAR-2024', '15:00', '0', '0');
+INSERT INTO Fixtures (FixtureID, Team1, Team2, OddsTeam1, OddsTeam2,
+Fdate, FTime, Score1, Score2)
+VALUES
+('3', 'Chelsea', 'Burnley', 1.50, 3.00,
+'30-MAR-2024', '15:00', '0', '0');
+INSERT INTO Fixtures (FixtureID, Team1, Team2, OddsTeam1, OddsTeam2,
+Fdate, FTime, Score1, Score2)
+VALUES
+('4', 'Nottingham Forest', 'Crystal Palace', 1.80, 2.50,
+'30-MAR-2024', '15:00', '0', '0');
+
+--Populating bets table
+INSERT INTO Bets (BetID, BetAmmount, BetOdds, BetStatus, BetDate, Team, FixtureID,
+AccountID)
+VALUES
+('1', 5, 1.50, 'p', '30-Jan-2021', 'Chelsea', '3', '4');
+INSERT INTO Bets (BetID, BetAmmount, BetOdds, BetStatus, Team, FixtureID,
+AccountID)
+VALUES
+('2', 20, 1.69, 'p', 'Bournemouth', '2', '5');
+INSERT INTO Bets (BetID, BetAmmount, BetOdds, BetStatus, Team, FixtureID,
+AccountID)
+VALUES
+('3', 10, 2.50, 'c', 'Crystal Palace', '4', '1');
+INSERT INTO Bets (BetID, BetAmmount, BetOdds, BetStatus, Team, FixtureID,
+AccountID)
+VALUES
+('4', 500, 3.33, 'p', 'West Ham United', '1', '3');
+
+
+COMMIT;
